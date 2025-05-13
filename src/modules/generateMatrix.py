52b4e3acac516630525ff18537d36dba713cc65e
riancmd <rian.dssantos@gmail.com>
2025-05-13 11:33:43 -0300

    fix ✅: multiplication working fine, added script for BitstoDecMatrix

---
+++ src/modules/generateMatrix.py
@@ -0,0 +1,35 @@
+import os
+
+def generateMenu():
+    matrizz = input('Digite a matriz completa: ')
+    os.system('cls')
+    matrixx = gerarMatriz(matrizz)
+
+def gerarMatriz(matriz):
+    matrix = [[0]*5 for _ in range(5)]
+    begin = 0
+    end = 8
+    for i in range(5):
+        print("|    ", end="")
+        for j in range(5):
+            bits = matriz[begin:end]
+            matrix[i][j] = bits
+            print(binaryToDec(bits), end="    ")
+            begin += 8
+            end += 8
+        print("|")
+    return matrix
+
+def binaryToDec(n):
+    val = int(n, 2)
+    if n[0] == '1':  # bit de sinal
+        val -= 256
+    return val
+
+while True:
+    generateMenu()
+    continuar = input('Deseja continuar? (S/N): ')
+    if continuar.upper() == 'S':
+        os.system('cls')
+    else:
+        break
