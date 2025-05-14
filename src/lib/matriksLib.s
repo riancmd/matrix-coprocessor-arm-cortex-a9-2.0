e026a74bff6ab49cf89a8b78327b94ee1a65e2df
riancmd <rian.dssantos@gmail.com>
2025-05-14 13:51:56 -0300

    added constants and some desc

--- src/lib/matriksLib.s
+++ src/lib/matriksLib.s
@@ -1,36 +1,63 @@
+@ Biblioteca em Assembly p comunicação com o coprocessador aritmético de matrizes
+@ São utilizadas 5 PIOs para a comnunicação, são elas:
+@ - PIO1
+@ - PIO2
+@ - PIO3
+@ - PIO4
+@ - PIO5
+
+@ Definição de constantes para as syscalls e stdin/o
+.equ EXIT,  1
+.equ READ,  3
+.equ WRITE, 4
+.equ OPEN,  5
+.equ CLOSE, 6
+
+.equ STDI, 0 @ Standard input
+.equ STDO, 1 @ Standard output
+
+@ Definição dos endereços físicos da De1-SoC
+.equ FPGA_BRIDGE_PHYS,  0xFF200  @ Endereço físico base
+.equ PIO_OUTPUT_OFFSET, 0x0000      @ Offset do PIO de saída (HPS->FPGA)
+.equ PIO_INPUT_OFFSET,  0x0010      @ Offset do PIO de entrada (FPGA->HPS)
+.equ PAGE_SIZE,         4096        @ Tamanho da página
+
+@ Constantes para o mmap
+
+
 .global _start
 
 _start:
     @ Exibe informações do menu
-    MOV R0,#1 @standard output
+    MOV R0,STDO @standard output
     LDR R1,=menu1 @ Guarda valor da string
     LDR R2,=len1
-    MOV R7,#4 @ Syscall: write
+    MOV R7,WRITE @ Syscall: write
     SWI 0
 
     LDR R1,=menu2
     LDR R2,=len2
-    MOV R7,#4 @ Syscall: write
+    MOV R7,WRITE @ Syscall: write
     SWI 0
 
     LDR R1,=menu3
     LDR R2,=len3
-    MOV R7,#4 @ Syscall: write
+    MOV R7,WRITE @ Syscall: write
     SWI 0
 
     LDR R1,=menu4
     LDR R2,=len4
-    MOV R7,#4 @ Syscall: write
+    MOV R7,WRITE @ Syscall: write
     SWI 0
 
     LDR R1,=menu5
     LDR R2,=len5
-    MOV R7,#4 @ Syscall: write
+    MOV R7,WRITE @ Syscall: write
     SWI 0
 
     @Recebe input do usuário
-    mov r7,#3 @syscall: read
-    mov r0,#0 @standard input
+    mov r7,READ @syscall: read
+    mov r0,STDI @standard input
     ldr r1,=buffer
     mov r2,#4
     svc #0
@@ -42,14 +69,27 @@ _start:
 
     
 .data //Seção de dados
-buffer: .space 4
-menu1: .string "|*****| matriks |*****|\n" //o mesmo que .asciz
-len1 = .-menu1
-menu2: .string "(1) Operação\n"
-len2 = .-menu2
-menu3: .string "(2) Sair\n"
-len3 = .-menu3
-menu4: .string "Operações: (1) Soma, (2) Subtração, (3) Multiplicação de matrizes\n"
-len4 = .-menu4
-menu5: .string "(4) Multiplicação por inteiro, (5) Determinante, (6) Transposta, (7) Oposta\n"
-len5 = .-menu5
+dev_mem:
+    .asciz "/dev/mem"
+buffer: 
+    .space 4
+menu1: 
+    .string "|*****| matriks |*****|\n" //o mesmo que .asciz
+len1 = 
+    .-menu1
+menu2: 
+    .string "(1) Operação\n"
+len2 = 
+    .-menu2
+menu3:  
+    .string "(2) Sair\n"
+len3 = 
+    .-menu3
+menu4: 
+    .string "Operações: (1) Soma, (2) Subtração, (3) Multiplicação de matrizes\n"
+len4 = 
+    .-menu4
+menu5: 
+    .string "(4) Multiplicação por inteiro, (5) Determinante, (6) Transposta, (7) Oposta\n"
+len5 = 
+    .-menu5
