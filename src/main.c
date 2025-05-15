afd48e59dea7e5b9133adbf529216be0cb3482ab
riancmd <rian.dssantos@gmail.com>
2025-05-15 14:57:35 -0300

    added user input for matrix A

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ void menuOperation();
 
 int main(){
     //Compilar utilizando gcc -finput-charset=UTF-8 antes do output para sair os caracteres
+    SetConsoleOutputCP(65001);  // Força a saída como UTF-8
     showMenu();
     return 0;
 }
@@ -17,8 +18,9 @@ void showMenu(){
     char* option;
     option = (char *)malloc(51*sizeof(char));
     //Aloca espaço para matriz
-    int* matrix;
-    matrix = (int *)malloc(25*sizeof(int));
+    int* matrixA, matrixB;
+    matrixA = (int *)malloc(25*sizeof(int));
+    matrixB = (int *)malloc(25*sizeof(int));
     
     while(1){
         printf("                ___  ___   __  __         _          _  _           ___  ___\n");
@@ -43,9 +45,9 @@ void showMenu(){
         printf("\n");
         usleep(250000);
         printf("(1) - Realizar operação");
-        printf("(2) - Sair");
+        printf("\n(2) - Sair");
         printf("\n");
-        printf("Digite uma opção: ");
+        printf("\nDigite uma opção: ");
         scanf("%s", option);
         
         if (option == NULL || ((*option) != '2' && (*option) != '1')){
@@ -55,19 +57,35 @@ void showMenu(){
         }
         
         switch (*option){
-            case '1': menuOperation(option);
+            case '1': menuOperation(option, matrixA, matrixB);
             case '2': exit(0);
         }
     }
 
 }
 
-void menuOperation(char* option){
-    clear();
-    printf("Operações:\n(1) Soma\n(2) Subtração\n(3) Multiplicação de matrizes\n(4) Multiplicação por inteiro\n(5) Determinante\n(6) Transposta\n(7) Oposta\n");
-    printf("Digite uma opção: ");
+void menuOperation(char* option, int* matrixA, int* matrixB){
+    int qty; // variável guarda se op precisa de 2 ou 1 matriz
+    int size;
+    int i,j; // iteradores
+
+    //clear();
+    printf("\n\nOperações:\n\n(1) Soma\n(2) Subtração\n(3) Multiplicação de matrizes\n(4) Multiplicação por inteiro\n(5) Determinante\n(6) Transposta\n(7) Oposta\n");
+    printf("\nDigite uma opção: ");
     scanf("%s", option);
 
+    qty = ((*option) > 4) ? 1 : 2; // verifica qtd de matrizes
+
+    printf("\nQual o tamanho da matriz?: ");
+    scanf("%d", &size);
+
+    printf("\nDigite a(s) matriz(es) em ordem de M[i,j].\n\n");
+
+    // recebe matrizes
+    for(i = 0; i < (size*size); i++){
+        printf("matriz[%d][%d]: ", (i/size), (i%size)); // printa a posição do elemento
+        scanf("%d", &(matrixA[i]));
+    }
 }
 
 void clear(){
