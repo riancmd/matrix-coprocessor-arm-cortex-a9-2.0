6f09b86714011a2cb8928645487f0004af6c945a
riancmd <rian.dssantos@gmail.com>
2025-05-15 15:32:47 -0300

    fixed prototypes and added new logic

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 /*
             Para compilar corretamente, é necessário utilizar a seguinte linha no terminal (windows):
             gcc -finput-charset=UTF-8 main.c resources\icon.o -o meu_programa.exe
+
+            LEMBRE: Criar header depois
 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 void showMenu();
-void clear();
-void menuOperation();
-void printarMatriz();
+void clean();
+void menuOperation(char* option, int* matrixA, int* matrixB);
+void printarMatriz(int* matriz, int size);
 
 int main(){
     //Compilar utilizando gcc -finput-charset=UTF-8 antes do output para sair os caracteres
@@ -22,12 +24,14 @@ void showMenu(){
     //Aloca espaço para opcao
     char* option;
     option = (char *)malloc(51*sizeof(char));
-    //Aloca espaço para matriz
+
+    //Aloca espaço para matrizes
     int* matrixA, matrixB;
     matrixA = (int *)malloc(25*sizeof(int));
     matrixB = (int *)malloc(25*sizeof(int));
     
     while(1){
+        //LEMBRETE: Trocar usleep por nanosleep para usar no Linux
         printf("                ___  ___   __  __         _          _  _           ___  ___\n");
         usleep(250000);
         printf("               |  _||  _| |  \\/  |       | |        (_)| |         |_  ||_  |\n");
@@ -57,7 +61,7 @@ void showMenu(){
         
         if (option == NULL || ((*option) != '2' && (*option) != '1')){
             printf("\n Opção inválida.\n");
-            //clear();
+            clean();
             continue;
         }
         
@@ -74,7 +78,7 @@ void menuOperation(char* option, int* matrixA, int* matrixB){
     int size;
     int i,j; // iteradores
 
-    //clear();
+    clean();
     printf("\n\nOperações:\n\n(1) Soma\n(2) Subtração\n(3) Multiplicação de matrizes\n(4) Multiplicação por inteiro\n(5) Determinante\n(6) Transposta\n(7) Oposta\n");
     printf("\nDigite uma opção: ");
     scanf("%s", option);
@@ -88,8 +92,18 @@ void menuOperation(char* option, int* matrixA, int* matrixB){
 
     // recebe matrizes
     for(i = 0; i < (size*size); i++){
-        printf("matriz[%d][%d]: ", (i/size), (i%size)); // printa a posição do elemento
+        printf("matrizA[%d][%d]: ", (i/size), (i%size)); // printa a posição do elemento
         scanf("%d", &(matrixA[i]));
+    }
+
+    if((*option) == 4){
+        printf("\n Digite o número: ");
+        scanf("%d", matrixB[0]);
+    }else if (qty == 2){
+        for(i = 0; i < (size*size); i++){
+        printf("matrizB[%d][%d]: ", (i/size), (i%size)); // printa a posição do elemento
+        scanf("%d", &(matrixB[i]));
+        }
     }
 }
 
@@ -97,12 +111,14 @@ void printarMatriz(int* matriz, int size){
     int i, count;
     count = 0;
 
-    for (i = 0; i < (size*size); i){
+    for (i = 0; i < (size*size); i++){
         if ((i+1)%size == 0) printf("\n");
-        printf("    %d    ", matriz[i]);
+        printf("    %3d    ", matriz[i]);
     }
+
+    printf("\n");
 }
 
-void clear(){
+void clean(){
     system("clear");
 }
