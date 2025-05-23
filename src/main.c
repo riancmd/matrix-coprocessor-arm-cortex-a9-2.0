3c11c9e69a6e246cae5fca981483014419571354
riancmd <rian.dssantos@gmail.com>
2025-05-23 00:54:34 -0300

    added tutorial / changed makefile

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 /*
-            Para compilar e rodar corretamente, é necessário utilizar as seguintes linhas no terminal:
+            Para compilar e rodar corretamente, é necessário utilizar as seguintes linhas no terminal, com o makefile:
+                make compile
+                make run
+
+            A primeira linha compila o programa, já a segunda, executa
+
+            Para compilar e montar separadamente assembly e código C, siga os seguintes passos:
                 as -c lib/matriksLib.s -o lib/matriksLib.o  [MONTA O ASSEMBLY]
                 gcc -c lib/functions.c -o lib/functions.o -I./lib [COMPILA O FUNCTIONS]
                 gcc -c main.c -o main.o -I./lib [COMPILA A MAIN]
                 gcc main.o lib/functions.o lib/matriksLib.o -o main [LINKA TUDO]
                 sudo ./main [RODA A MAIN]
-
-            gcc -finput-charset=UTF-8 main.c resources\icon.o -o main.exe [WINDOWS, IGNORE]
 */
 #include <stdlib.h>
 #include <stdio.h>
